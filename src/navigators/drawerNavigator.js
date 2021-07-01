0f1d2c7d92b9e631f1c500a6e31b82041cfcee82
grandbusta <bustajay30@gmail.com>
2021-07-01 17:46:49 +0100

    navigation updated

--- src/navigators/drawerNavigator.js
+++ src/navigators/drawerNavigator.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import Reminder from '../screens/reminder';
 import HomeStack from './homeStack';
 import {Colors} from '../shared/colors';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  const {navigation} = props;
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label="Home"
+        labelStyle={{color: Colors.white}}
+        onPress={() => {
+          navigation.navigate('HomeStack');
+        }}
+      />
+      <DrawerItem
+        label="Reminder"
+        labelStyle={{color: Colors.white}}
+        onPress={() => {
+          navigation.navigate('Reminder');
+        }}
+        activeBackgroundColor="blue"
+      />
+    </DrawerContentScrollView>
+  );
+}
 
 export default function DrawerNavigator() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerStyle={{backgroundColor: Colors.primary.normal}}>
+      <Drawer.Navigator
+        drawerContentOptions={{
+          activeBackgroundColor: Colors.primary.light,
+          contentContainerStyle: {paddingHorizontal: 0},
+          itemStyle: {paddingHorizontal: 10, marginHorizontal: 0},
+          labelStyle: {color: 'white'},
+        }}
+        drawerStyle={{backgroundColor: Colors.primary.normal}}>
         <Drawer.Screen
           name="HomeStack"
-          options={{title: 'Home'}}
+          options={{
+            title: 'Home',
+            drawerIcon: () => (
+              <MaterialIcons
+                name="calendar-today"
+                color={Colors.lightgrey}
+                size={26}
+              />
+            ),
+          }}
           component={HomeStack}
         />
         <Drawer.Screen name="Reminder" component={Reminder} />
